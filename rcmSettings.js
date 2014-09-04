72612b195b63a7e2bc43d94d230d3c731d53e1d5
afrendybayu <afrendy@gmail.com>
2014-09-04 11:47:38 +0700

    settting

---
+++ rcmSettings.js
@@ -0,0 +1,17 @@
+rcmSettings = {
+	eqx: 0,
+	tgl: '0',
+	cat: 5,
+	//tab: 'tu_re',
+	//tab: 'tu_sap',
+	tab: 'tu_ho',
+	//tab: 'tu_sap',
+	//tab: 'tu_rh',
+	//tab: 'tu_kf',
+	tsp: 'tu_ho',
+	//tsp: 'ts_pdm',
+	asa: '',
+	sas: '',
+	server: 1
+};
+
